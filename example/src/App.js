e9316dc408eec4f09b13f5e5c13c478ea8b85d3d
catman0745 <54468544+catman0745@users.noreply.github.com>
2021-07-21 15:27:52 +0300

    Reformat everything with esling and prettier

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'modsen-toast'
 import 'modsen-toast/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
