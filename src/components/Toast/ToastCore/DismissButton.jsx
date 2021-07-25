58b7b142e97afeb93b0e2649e1e7525df56b17f3
catman0745 <54468544+catman0745@users.noreply.github.com>
2021-07-25 12:14:06 +0300

    Refactor toast dismiss icon handing

--- src/components/Toast/ToastCore/DismissButton.jsx
+++ src/components/Toast/ToastCore/DismissButton.jsx
@@ -2,21 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 
-const DismissIcon = ({ color, style, ...props }) => (
-  <svg
-    {...props}
-    style={{ ...style, fill: color }}
-    viewBox="0 0 32 32"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g transform="translate(0 -0.001)">
-      <path
-        className="a"
-        d="M18.83,16,31.414,3.417a2,2,0,0,0-2.83-2.83L16,13.172,3.416.587a2,2,0,0,0-2.83,2.83L13.171,16,.586,28.585a2,2,0,1,0,2.83,2.829L16,18.83,28.584,31.415a2,2,0,0,0,2.83-2.83Z"
-      />
-    </g>
-  </svg>
-)
+import DismissIcon from '../assets/dismiss.svg'
 
 const DismissButtonBody = styled.div`
   position: absolute;
@@ -28,7 +14,7 @@ const DismissButtonBody = styled.div`
 
 const DismissButton = ({ color, ...props }) => (
   <DismissButtonBody {...props}>
-    <DismissIcon color={color} />
+    <DismissIcon style={{ fill: color }} />
   </DismissButtonBody>
 )
 DismissButton.propTypes = {
