1156192e93fe3c7ab6351c5e625afe82b3e11c77
catman0745 <54468544+catman0745@users.noreply.github.com>
2021-07-28 18:32:20 +0300

    Move toast transitions into ToastsList component

--- src/components/ToastsStack/ToastsList/ToastsList.jsx
+++ src/components/ToastsStack/ToastsList/ToastsList.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { animated } from 'react-spring'
+import { animated, useTransition } from 'react-spring'
 
 import Toast from '@components/Toast'
 import { useConfiguration } from '@components/Configuration'
 
-import useSlidingTransition from './useSlidingTransition'
 import useNotifications from './useNotifications'
+
+const direction = (side) => {
+  switch (side) {
+    case 'right':
+      return 1
+    case 'left':
+      return -1
+    default:
+      throw new Error('Unsupported side option.')
+  }
+}
 
 const AnimatedToast = animated(Toast)
 
@@ -13,12 +23,21 @@ const ToastsList = () => {
   const notifications = useNotifications()
 
   const { slidingSide } = useConfiguration()
-  const transitions = useSlidingTransition(notifications, slidingSide)
+  const dir = direction(slidingSide)
 
-  return transitions((style, { id, type, text, dismiss }) => (
+  const transitions = useTransition(notifications, {
+    from: { opacity: 0, x: 1 },
+    enter: { opacity: 1, x: 0 },
+    leave: { opacity: 0, x: 1 }
+  })
+
+  return transitions(({ x, ...style }, { id, type, text, dismiss }) => (
     <AnimatedToast
       key={id}
-      style={style}
+      style={{
+        transform: x.to((v) => `translateX(calc(${v * dir} * 150%))`),
+        ...style
+      }}
       type={type}
       text={text}
       dismiss={() => dismiss()}
